d236cd387c36572d341f28fcb784315f2cfd924d
mnusrullah104 <133938501+mnusrullah104@users.noreply.github.com>
2024-07-13 16:02:36 +0500

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Number Guessing Game CLI
+
+Experience the excitement of guessing a number in this Command Line Interface (CLI) game! Built using TypeScript, this game challenges players to guess a randomly generated number within a specified range. It's a fun and interactive way to test your guessing skills directly from your terminal.
---
+++ index.ts
@@ -0,0 +1,21 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+
+// compare user input from computer genrated input
+const randomNumber=Math.floor(Math.random()* 6+1);
+const answers =await inquirer.prompt(
+{
+name:   "userGuessNumber",
+type:  "number",
+message:    "please guess a number between 1-10:",
+
+}
+);
+
+if(answers.userGuessNumber===randomNumber){
+console.log("Congratulations! you guess right number")
+}
+else{
+    console.log( "You guessed wrong number")
+}
+console.log(answers)
